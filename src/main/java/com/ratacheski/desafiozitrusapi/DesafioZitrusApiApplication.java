655661963fa5536c84dc64f9c014adb73025e2e0
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2021-10-23 09:39:41 -0300

    Commit Inicial da Criação do Projeto Spring

---
+++ src/main/java/com/ratacheski/desafiozitrusapi/DesafioZitrusApiApplication.java
@@ -0,0 +1,13 @@
+package com.ratacheski.desafiozitrusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioZitrusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioZitrusApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ratacheski/desafiozitrusapi/DesafioZitrusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratacheski.desafiozitrusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZitrusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
