ee8a380fa306676383c7f57eff0cd01de971b011
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2021-10-23 15:25:08 -0300

    Ajuste do parâmetro de mapeamento do mapStruct

--- src/main/java/com/ratacheski/desafiozitrusapi/api/controller/ProdutoController.java
+++ src/main/java/com/ratacheski/desafiozitrusapi/api/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package com.ratacheski.desafiozitrusapi.api.controller;
 
 import com.ratacheski.desafiozitrusapi.api.dto.ProdutoDTO;
 import com.ratacheski.desafiozitrusapi.api.mapper.MapStructMapper;
+import com.ratacheski.desafiozitrusapi.domain.service.ProdutoService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,13 +15,17 @@ public class ProdutoController {
 
     private final MapStructMapper mapStructMapper;
 
-    public ProdutoController(MapStructMapper mapStructMapper) {
+    private final ProdutoService produtoService;
+
+    public ProdutoController(MapStructMapper mapStructMapper, ProdutoService produtoService) {
         this.mapStructMapper = mapStructMapper;
+        this.produtoService = produtoService;
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
     public ProdutoDTO criar(@Valid @RequestBody ProdutoDTO produtoDTO) {
-        return null;
+        var produto = mapStructMapper.ProdutoDtoToProduto(produtoDTO);
+        return mapStructMapper.ProdutoToProdutoDto(produtoService.criar(produto));
     }
 }
