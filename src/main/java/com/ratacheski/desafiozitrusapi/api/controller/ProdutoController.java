e0acbc8e5291df72eab53948869369207bac3697
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2021-10-23 15:45:28 -0300

    Criação do endpoint de listar produtos

--- src/main/java/com/ratacheski/desafiozitrusapi/api/controller/ProdutoController.java
+++ src/main/java/com/ratacheski/desafiozitrusapi/api/controller/ProdutoController.java
@@ -2,10 +2,14 @@ package com.ratacheski.desafiozitrusapi.api.controller;
 
 import com.ratacheski.desafiozitrusapi.api.dto.ProdutoDTO;
 import com.ratacheski.desafiozitrusapi.api.mapper.MapStructMapper;
+import com.ratacheski.desafiozitrusapi.domain.model.Produto;
 import com.ratacheski.desafiozitrusapi.domain.service.ProdutoService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -20,6 +24,15 @@ public class ProdutoController {
     public ProdutoController(MapStructMapper mapStructMapper, ProdutoService produtoService) {
         this.mapStructMapper = mapStructMapper;
         this.produtoService = produtoService;
+    }
+
+    @GetMapping
+    public List<ProdutoDTO> listar() {
+        var produtos = produtoService.listar();
+        return produtos
+                .stream()
+                .map(produto -> mapStructMapper.ProdutoToProdutoDto(produto))
+                .collect(Collectors.toList());
     }
 
     @PostMapping
